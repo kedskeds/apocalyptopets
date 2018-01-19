1e9f305388b8ed86fbec4bd885356755b2950ca5
Christine Duane <ckduane@protonmail.com>
2018-01-18 16:57:35 -0800

    add goat and two eyeball choices

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,85 +1,12 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <div class="image">
+      <img src="../assets/goat.svg" alt="Goat Pet" class="goat">
+      <img src="../assets/goat-eye-1.svg" alt="Goat Eye Option 1" class="goat-eye option-1">
+      <img src="../assets/goat-eye-2.svg" alt="Goat Eye Option 1" class="goat-eye option-2">
+    </div>
+    <button v-on:click="decode">Decode</button>
   </div>
 </template>
 
@@ -88,14 +15,22 @@ export default {
   name: 'HelloWorld',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
-    };
+      msg: 'Turn back before it\'s too late'.split('').reverse().join(''),
+    }
   },
+  methods: {
+    decode: function(){
+      this.msg = this.msg.split('').reverse().join('')
+    }
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+html {
+  font-size: 62.5%;
+}
 h1, h2 {
   font-weight: normal;
 }
@@ -109,5 +44,22 @@ li {
 }
 a {
   color: #42b983;
+}
+.hide {
+  display: none;
+}
+.image {
+  position: relative;
+}
+.goat, .goat-eye {
+  height: 20rem;
+}
+.goat-eye {
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+}
+.goat-horn {
+  fill: blue;
 }
 </style>
