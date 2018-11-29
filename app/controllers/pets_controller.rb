be64a221f7d9535844a4ace55f5e188be96531ad
Christine Duane <ckduane@protonmail.com>
2018-11-29 09:59:24 -0800

    update form, pet model

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,13 +2,22 @@ class PetsController < ApplicationController
 
   def new
     @pet = Pet.new
+    @species = Species.all
     @colors = Color.all
     @stats = Stat.all
   end
 
   def create
     @pet = Pet.new(pet_params)
+    @species = Species.all
+    @colors = Color.all
+    @stats = Stat.all
     if @pet.save
+      @pet.pet_stats.create(
+        pet_id: @pet.id,
+        stat_id: @stat.id
+        )
+      flash[:notice] = "Pet was saved"
       redirect_to pet_path(@pet)
     else
       @errors = @pet.errors.full_messages
@@ -25,7 +34,7 @@ class PetsController < ApplicationController
   private
 
   def pet_params
-    params.require(:pet).permit(:user_id, :pet_id, :color_id)
+    params.require(:pet).permit(:user_id, :pet_id, :species_id, :color_id, :name)
   end
 
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,11 +1,10 @@
 class Pet < ApplicationRecord
   belongs_to :user
-  belongs_to :species
 
   has_many :accessories
   has_many :pet_stats
   has_many :stats, through: :pet_stats
 
-  validates_associated :user, :species
+  validates_associated :user
   validates :name, uniqueness: true
 end
